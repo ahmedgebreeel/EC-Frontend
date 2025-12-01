c680020767bb655876cd78870c252a868916ded7
mohamedsamy3110 <memo.samy3110@gmail.com>
2025-12-01 19:31:28 +0200

    Create about page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,5 +13,9 @@ export const routes: Routes = [
         path: 'home',
         loadComponent: () => import('./features/home/home').then(m => m.Home)
     },
+    {
+        path: 'about',
+        loadComponent: () => import('./features/about/about').then(m => m.About)
+    },
 
 ];
