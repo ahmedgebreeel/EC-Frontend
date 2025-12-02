f47035f3233467a540dac7e6ed893e0508e4b018
mohamedsamy3110 <memo.samy3110@gmail.com>
2025-12-02 23:00:05 +0200

    Create Product Admin Panel

---
+++ src/app/admin/product/getall/getall.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+
+@Component({
+  selector: 'app-getall',
+  imports: [RouterLink],
+  templateUrl: './getall.html',
+  styleUrl: './getall.css',
+})
+export class Getall {
+
+}
