033f61fb5ab25cfdb4ce29c182c5f2d5a707a06f
ahmed gebreel <ahmedgebreeeel@gmail.com>
2025-12-02 19:34:24 +0200

    configure main-layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,44 +3,49 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     {
         path: '',
-        redirectTo: 'home',
-        pathMatch: 'full'
+        loadComponent: () => import('./layouts/main-layout/main-layout').then(m => m.MainLayout),
+        children: [
+            {
+                path: '',
+                redirectTo: 'home',
+                pathMatch: 'full'
+            },
+            {
+                path: 'home',
+                loadComponent: () => import('./features/home/home').then(m => m.Home)
+            },
+            {
+                path: 'register',
+                loadComponent: () => import('./features/auth/register/register').then(m => m.Register)
+            },
+            {
+                path: "login",
+                loadComponent: () => import('./features/auth/login/login').then(m => m.Login)
+            },
+            {
+                path: "cart",
+                loadComponent: () => import('./features/cart/cart').then(m => m.Cart)
+            },
+            {
+                path: "checkout",
+                loadComponent: () => import('./features/checkout/checkout').then(m => m.Checkout)
+            },
+            {
+                path: "about",
+                loadComponent: () => import('./features/about/about').then(m => m.About)
+            },
+            {
+                path: "contact-us",
+                loadComponent: () => import('./features/contact/contact').then(m => m.Contact)
+            },
+            {
+                path: "faq",
+                loadComponent: () => import('./features/faq/faq').then(m => m.Faq)
+            },
+            {
+                path: '**',
+                loadComponent: () => import('./features/not-found/not-found').then(m => m.NotFound)
+            }
+        ]
     },
-    {
-        path: 'register',
-        loadComponent: () => import('./features/auth/register/register').then(m => m.Register)
-    },
-    {
-        path: 'login',
-        loadComponent: () => import('./features/auth/login/login').then(m => m.Login)
-    },
-    {
-        path: 'home',
-        loadComponent: () => import('./features/home/home').then(m => m.Home)
-    },
-    {
-        path: 'about',
-        loadComponent: () => import('./features/about/about').then(m => m.About)
-    },
-    {
-        path: 'cart',
-        loadComponent: () => import('./features/cart/cart').then(m => m.Cart)
-    },
-    {
-        path: 'checkout',
-        loadComponent: () => import('./features/checkout/checkout').then(m => m.Checkout)
-    },
-    {
-        path: 'contact',
-        loadComponent: () => import('./features/contact/contact').then(m => m.Contact)
-    },
-    {
-        path: 'faq',
-        loadComponent: () => import('./features/faq/faq').then(m => m.Faq)
-    },
-    {
-        path: '**',
-        loadComponent: () => import('./features/not-found/not-found').then(m => m.NotFound)
-    },
-
 ];
