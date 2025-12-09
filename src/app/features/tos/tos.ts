02022c725526fb42f646df5adff6e5a95061b628
mohamedsamy3110 <memo.samy3110@gmail.com>
2025-12-09 19:15:48 +0200

    Add terms of service page

---
+++ src/app/features/tos/tos.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+
+@Component({
+  selector: 'app-tos',
+  imports: [RouterLink],
+  templateUrl: './tos.html',
+  styleUrl: './tos.css',
+})
+export class Tos {
+
+}
